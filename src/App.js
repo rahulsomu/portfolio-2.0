f25eaf698edd9d47d35009f1f778e61414a3c5d7
Rahul <rana.rahul.1998@gmail.com>
2022-11-25 15:36:57 +0530

    added project data

--- src/App.js
+++ src/App.js
@@ -7,36 +7,61 @@ import Reviews from "./Components/Projects/Reviews/Reviews";
 import Resume from "./Components/Resume/Resume";
 import Navbar from "./Components/UI/Navbar";
 import Wink from "./Components/UI/Wink";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [splashView, setSplashView] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSplashView(false);
+    }, 3000);
+  }, []);
   const date = new Date();
   let year = date.getFullYear();
   return (
     <div className="App">
-      <Navbar />
-      <section id="home">
-        <Hero />
-      </section>
-      <section id="about">
-        <About />
-      </section>
-      <section id="projects">
-        <Projects />
-        <Reviews />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-      <section id="resume">
-        <Resume />
-      </section>
-      <footer>
-        <p>Thanks for Visiting!</p>
-        <div className="wink2">
-          <Wink />
-        </div>
-        <p className="copyright">&copy;{` Rahul Rana ${year}`}</p>
-      </footer>
+      {splashView ? (
+        <motion.div className="splash_screen">
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            Welcome .
+          </motion.p>
+        </motion.div>
+      ) : (
+        <>
+          <Navbar />
+          <section id="home">
+            <Hero />
+          </section>
+          <section id="about">
+            <About />
+          </section>
+
+          <section id="projects">
+            <Projects />
+          </section>
+
+          <Reviews />
+          <section id="contact">
+            <Contact />
+          </section>
+          <section id="resume">
+            <Resume />
+          </section>
+          <footer>
+            <p>Thanks for Visiting!</p>
+            <div className="wink2">
+              <Wink />
+            </div>
+            <p className="copyright">&copy;{` Rahul Rana ${year}`}</p>
+          </footer>
+        </>
+      )}
     </div>
   );
 }
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./projects.css";
-import { projectData } from "./Reviews/projectData";
+import { projectData } from "./projectData";
 import { motion } from "framer-motion";
 
 const Projects = () => {
+  const [viewAll, setViewAll] = useState(false);
+
   return (
     <div className="projects_section">
       <motion.div
@@ -16,17 +18,66 @@ const Projects = () => {
       </motion.div>
       <div className="projects_section_content">
         <div className="content_header">
-          <p>Some Featured Projects</p>
-          <button>View All</button>
+          <p>{viewAll ? "All Projects" : "Some Featured Projects"}</p>
+          <button onClick={() => setViewAll(!viewAll)}>
+            {!viewAll ? "View All" : "View Less"}
+          </button>
         </div>
-        <div className="projects_grid">
-          {projectData
-            .filter((item) => item.featured === true)
-            .map((project) => {
+
+        {!viewAll ? (
+          <div className="projects_grid">
+            {projectData
+              .filter((item) => item.featured === true)
+              .map((project) => {
+                return (
+                  <motion.div
+                    key={project.id}
+                    className="grid_item"
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                    viewport={{ once: true }}
+                  >
+                    <p className="details_info">Click to View Details</p>
+                    <div className="project_image">
+                      <img src={project.image} alt="project" srcset="" />
+                    </div>
+                    <div className="project_info">
+                      <div>
+                        <div className="project_name">
+                          <p>{project.name}</p>
+                        </div>
+                        <div className="project_desc">
+                          <p>{project.description}</p>
+                        </div>
+                        <div className="tags_container">
+                          {project.tags.map((tag) => (
+                            <div className="tag">
+                              <p>{tag}</p>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+
+                      <div className="preview_button">
+                        <button>
+                          <a href={project.link} target="_">
+                            Visit
+                          </a>
+                        </button>
+                      </div>
+                    </div>
+                  </motion.div>
+                );
+              })}
+          </div>
+        ) : (
+          <div className="projects_grid_small">
+            {projectData.map((project) => {
               return (
                 <motion.div
                   key={project.id}
-                  className="grid_item"
+                  className="grid_item_small"
                   initial={{ opacity: 0, y: 100 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.5 }}
@@ -36,15 +87,15 @@ const Projects = () => {
                   <div className="project_image">
                     <img src={project.image} alt="project" srcset="" />
                   </div>
-                  <div className="project_info">
+                  <div className="project_info_small">
                     <div>
-                      <div className="project_name">
+                      <div className="project_name_small">
                         <p>{project.name}</p>
                       </div>
-                      <div className="project_desc">
+                      <div className="project_desc_small">
                         <p>{project.description}</p>
                       </div>
-                      <div className="tags_container">
+                      <div className="tags_container_small">
                         {project.tags.map((tag) => (
                           <div className="tag">
                             <p>{tag}</p>
@@ -53,7 +104,7 @@ const Projects = () => {
                       </div>
                     </div>
 
-                    <div className="preview_button">
+                    <div className="preview_button_small">
                       <button>
                         <a href={project.link} target="_">
                           Visit
@@ -64,113 +115,14 @@ const Projects = () => {
                 </motion.div>
               );
             })}
+          </div>
+        )}
 
-          {/* <motion.div
-            className="grid_item"
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.5 }}
-            viewport={{ once: true }}
-          >
-            <p className="details_info">Click to View Details</p>
-            <div className="project_image">
-              <img
-                src="https://images.unsplash.com/photo-1654161975496-065c5d93391c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
-                alt=""
-                srcset=""
-              />
-            </div>
-            <div className="project_info">
-              <div>
-                <div className="project_name">
-                  <p>Amazon Clone</p>
-                </div>
-                <div className="project_desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur. Pellentesque aenean
-                    orci in sagittis suspendisse. Facilisis eu tellus bibendum
-                    tempor vestibulum malesuada arcu.{" "}
-                  </p>
-                </div>
-              </div>
-
-              <div className="preview_button">
-                <button>Preview</button>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div
-            className="grid_item"
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-            viewport={{ once: true }}
-          >
-            <p className="details_info">Click to View Details</p>
-            <div className="project_image">
-              <img
-                src="https://images.unsplash.com/photo-1666805949083-b8b8698a347a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                alt=""
-                srcset=""
-              />
-            </div>
-            <div className="project_info">
-              <div>
-                <div className="project_name">
-                  <p>Amazon Clone</p>
-                </div>
-                <div className="project_desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur. Pellentesque aenean
-                    orci in sagittis suspendisse. Facilisis eu tellus bibendum
-                    tempor vestibulum malesuada arcu.{" "}
-                  </p>
-                </div>
-              </div>
-
-              <div className="preview_button">
-                <button>Preview</button>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div
-            className="grid_item"
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.5 }}
-            viewport={{ once: true }}
-          >
-            <p className="details_info">Click to View Details</p>
-            <div className="project_image">
-              <img
-                src="https://images.unsplash.com/photo-1661311850567-d2db65efe84a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-                alt=""
-                srcset=""
-              />
-            </div>
-            <div className="project_info">
-              <div>
-                <div className="project_name">
-                  <p>Amazon Clone</p>
-                </div>
-                <div className="project_desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur. Pellentesque aenean
-                    orci in sagittis suspendisse. Facilisis eu tellus bibendum
-                    tempor vestibulum malesuada arcu.{" "}
-                  </p>
-                </div>
-              </div>
-
-              <div className="preview_button">
-                <button>Preview</button>
-              </div>
-            </div>
-          </motion.div> */}
-        </div>
-        <div className="grid_footer">
-          <button>View All</button>
-        </div>
+        {/* <div className="grid_footer">
+          <button onClick={() => setViewAll(!viewAll)}>
+            <a href="#allProjects">{!viewAll ? "View All" : "View Less"}</a>
+          </button>
+        </div> */}
       </div>
     </div>
   );
